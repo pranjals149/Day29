a210334428565d45c760a489d531ded5229cc446
pranjals149 <42092291+pranjals149@users.noreply.github.com>
2020-05-21 22:12:52 +0530

    Day29

---
+++ routes/users.js
@@ -0,0 +1,38 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+var User = require('../models/user')
+var passport = require('passport');
+var authenticate = require('../authenticate');
+
+var router = express.Router();
+router.use(bodyParser.json())
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/signup', (req, res, next) => {
+  User.register(new User({ username: req.body.username }), req.body.password), (user, err) => {
+    if(err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json')
+      res.json({err: err})
+    } else {
+      passport.authenticate('local')(req, res, () => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json')
+        res.json({ success: true, status: 'Registration Successfull'})
+      })
+    } 
+  }
+})
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+
+  var token = authenticate.getToken({ _id: req.user._id });
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json')
+  res.json({ success: true, token: token, status: 'You are successfully logged in'})
+})
+
+module.exports = router;
